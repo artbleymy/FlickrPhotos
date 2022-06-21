413c30130175cf3859f9500bb72e8321d13c65b5
Stanislav Kozlov <bleymy@mail.ru>
2022-06-21 10:25:43 +0300

    Remove storyboard

---
+++ FlickrPhotos/Modules/ModulesFactory.swift
@@ -0,0 +1,24 @@
+//
+//  ModulesFactory.swift
+//  FlickrPhotos
+//
+//  Created by Stanislav on 21.06.2022.
+//
+
+import UIKit
+
+final class ModulesFactory {
+    lazy var navigationController = makeNavigationController()
+    
+    private lazy var feedModule = makeFeedViewController()
+    
+    private func makeNavigationController() -> UIViewController {
+        UINavigationController(rootViewController: feedModule)
+    }
+    
+    private func makeFeedViewController() -> UIViewController {
+        ViewController()
+    }
+    
+}
+
